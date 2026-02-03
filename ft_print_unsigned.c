aa18a6ca4ba50b5997fc6825b7dfedc094479acf
raqroca- <raqroca-@student.42madrid.com>
2026-02-03 16:57:00 +0100

    unsigned xd

---
+++ ft_print_unsigned.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_unsigned.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: raqroca- <raqroca-@student.42madrid.c      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/03 16:51:12 by raqroca-          #+#    #+#             */
+/*   Updated: 2026/02/03 16:51:15 by raqroca-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+int ft_print_unsigned(unsigned int n)
+{
+    unsigned int	len;
+
+	len = 0;
+	if (n >= 10)
+	{
+		len += ft_print_nbr(n / 10);
+	}
+	len += ft_print_char((n % 10) + '0'); 
+	return (len);
+}
